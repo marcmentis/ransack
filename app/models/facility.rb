2d61fa2e2787b4c17ad75ecf996c96cf051d9162
Marc Mentis <mmentis@optonline.net>
2014-09-22 11:08:44 -0400

    Refactor grouped_options

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,4 +1,3 @@
 class Facility < ActiveRecord::Base
 	has_many :pats
-	has_many :for_selects
 end
--- app/models/pat.rb
+++ app/models/pat.rb
@@ -1,6 +1,5 @@
 class Pat < ActiveRecord::Base
 	belongs_to :facility
-	has_many :for_selects, through: :facilities
 
 	#accepts_nested_attributes_for :for_selects
 end
