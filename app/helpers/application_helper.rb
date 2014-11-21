e084945c2df2313020d3a4cad302bb934bd9a374
Marc Mentis <mmentis@optonline.net>
2014-11-21 14:52:00 -0500

    Change application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,6 @@ module ApplicationHelper
 	def current_user
 		# User.find(session[:current_user_id])
 		# Needs to be an array not an ActiveRecord Relation
-		User.where('authen == ?', session[:authen]).first
+		User.where('authen = ?', session[:authen]).first
 	end
 end
